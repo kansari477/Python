3e71aa5aa34e2781db67dbd8cd1c0d5e998d4a81
kansari477 <kansari477@gmail.com>
2019-12-08 07:27:41 +0000

    First Commit

---
+++ sample.py
@@ -0,0 +1,9 @@
+print("this is sample file")
+a=2
+b=3
+c=a+b
+print(a)
+print("This is value of variable a", a)
+print("%s + %s = %s" %(a,b,c)) 
+print("This is value of variable a "+str(a)+ " and value for variable b")
+     
